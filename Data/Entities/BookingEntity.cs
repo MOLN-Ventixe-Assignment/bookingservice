80d07b7df3ec4760398a1a3aa79de80120e8742b
Åsa Stabler <asa.stabler@gmail.com>
2025-05-28 23:29:54 +0200

    Add project files.

---
+++ Data/Entities/BookingEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class BookingEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string EventId { get; set; } = null!;
+
+    public int TicketQuantity { get; set; } = 1;
+
+    //[Column(TypeName = "datetime2")]
+    public DateTime BookingDate { get; set; }
+
+    [ForeignKey(nameof(BookingOwner))]
+    public string? BookingOwnerId { get; set; }
+    public BookingOwnerEntity? BookingOwner  { get; set; }
+}
---
+++ Data/Entities/BookingOwnerEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class BookingOwnerEntity
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    [ForeignKey(nameof(Address))]
+    public string? BookingAddressId { get; set; }
+    public BookingAddressEntity? Address { get; set; }
+}
